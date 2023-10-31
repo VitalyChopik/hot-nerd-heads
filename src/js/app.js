63a0906a1774ba86bafaa38223d0c75e1693af7f
VitalyChopik <leo.space97@gmail.com>
2023-10-31 22:15:37 +0300

    feat: create website

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,5 @@
-/**
- * !(i)
- * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
- * Или когда импортирован весь файл, например import "files/script.js";
- * Неиспользуемый код в итоговый файл не попадает.
-
- * Если мы хотим добавить модуль следует его раскомментировать
- */
-// import MousePRLX from './libs/parallaxMouse'
-// import AOS from 'aos'
-// import Swiper, { Navigation, Pagination } from 'swiper';
 
 import BaseHelpers from './helpers/BaseHelpers.js';
-import PopupManager from './modules/PopupManager';
-import BurgerMenu from './modules/BurgerMenu';
-import Tabs from './modules/Tabs';
-import Accordion from './modules/Accordion.js';
 
 BaseHelpers.checkWebpSupport();
 
@@ -22,43 +7,29 @@ BaseHelpers.addTouchClass();
 
 BaseHelpers.addLoadedClass();
 
-BaseHelpers.headerFixed();
 
-/**
- * Открытие/закрытие модальных окон
- * Чтобы модальное окно открывалось и закрывалось
- * На окно повешай атрибут data-popup="<название окна>"
- * На кнопку, которая вызывает окно повешай атрибут data-type="<название окна>"
 
- * На обертку(.popup) окна добавь атрибут '[data-close-overlay]'
- * На кнопку для закрытия окна добавь класс '.button-close'
- * */
-new PopupManager();
 
-/**
- *  Модуль для работы с меню (Бургер)
- * */
-new BurgerMenu().init();
+const form = document.querySelector('.contact__form');
 
-/**
- *  Библиотека для анимаций
- *  документация: https://michalsnik.github.io/aos
- * */
-// AOS.init();
-
-/**
- * Параллакс мышей
- * */
-// new MousePRLX();
-
-new Tabs('tabs-example', {
-	onChange: (data) => {
-		console.log(data);
-	},
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	sendData();
 });
-
-new Accordion('.accordion', {
-	shouldOpenAll: false, // true
-	defaultOpen: [], // [0,1]
-	collapsedClass: 'open',
-});
+function sendData() {
+	document.querySelector('.contact__form-btn').textContent = 'Sending...';
+	const formData = new FormData(form);
+	fetch('https://formsubmit.co/chopikvitali@yandex.by', {
+		method: 'POST',
+		body: formData,
+	})
+		.then(response => response.text())
+		.then(data => {
+			form.classList.toggle('hide');
+			document.querySelector('.contact__titles').classList.toggle('hide');
+			document.querySelector('.contact__form-success').classList.toggle('active');
+		})
+		.catch(error => {
+			console.error('Error sending data:', error);
+		});
+}
